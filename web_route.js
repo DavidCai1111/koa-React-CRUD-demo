9247a575e466651a68d25dfb97c12fd162dfcd2b
DavidCai <376462191@qq.com>
2015-05-01 14:29:40 +0800

    提示信息

--- web_route.js
+++ web_route.js
@@ -1,25 +1,24 @@
 var router = require('koa-router')();
-var indexRoute = require('./routes/index');
-var authRoute = require('./routes/auth');
-var authService = require('./services/auth');
-var weiboRoute = require('./routes/weibo');
+var dr = require('dir-requirer')(__dirname);
+var routes = dr('./routes');
+var services = dr('./services');
 
 //主页
-router.get('/', indexRoute.showIndexPage);
+router.get('/', routes.index.showIndexPage);
 //进入注册页
-router.get('/signUp', authService.checkNotLogin, authRoute.showSignUpPage);
+router.get('/signUp', services.auth.checkNotLogin, routes.auth.showSignUpPage);
 //注册
-router.post('/signUp', authService.checkNotLogin, authRoute.doSignUp);
+router.post('/signUp', services.auth.checkNotLogin, routes.auth.doSignUp);
 //进入登陆页
-router.get('/login', authService.checkNotLogin, authRoute.showLoginPage);
+router.get('/login', services.auth.checkNotLogin, routes.auth.showLoginPage);
 //登陆
-router.post('/login', authService.checkNotLogin, authRoute.doLogin);
+router.post('/login', services.auth.checkNotLogin, routes.auth.doLogin);
 //登出
-router.get('/logout', authService.checkLogin, authRoute.doLogout);
+router.get('/logout', services.auth.checkLogin, routes.auth.doLogout);
 //新增一条微博
-router.post('/weibo/add', authService.checkLogin, weiboRoute.addNewWeibo);
+router.post('/weibo/add', services.auth.checkLogin, routes.weibo.addNewWeibo);
 //删除一条微博
-router.get('/weibo/delete/:id', authService.checkLogin ,weiboRoute.deleteOneWeibo);
+router.get('/weibo/delete/:id', services.auth.checkLogin ,routes.weibo.deleteOneWeibo);
 
 module.exports = router;
 
