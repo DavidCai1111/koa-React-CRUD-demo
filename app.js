8059ebaa819e6447d91547e3b7757f0cedd7f998
DavidCai <376462191@qq.com>
2015-05-02 09:57:36 +0800

    优化

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var mongoStore = require('koa-generic-session-mongo');
 var views = require('koa-views');
 var bodyParser = require('koa-bodyparser');
 var flash = require('koa-flash');
+var gzip = require('koa-gzip');
+var error = require('koa-error');
+var staticCache = require('koa-static-cache');
 
 app.keys = [config.appName];
 
@@ -16,13 +19,20 @@ app.use(session({
     db: config.db.name
   })
 }));
+app.use(error({
+  template: './views/error.html'
+}));
 app.use(bodyParser());
 app.use(flash());
 app.use(logger());
+app.use(staticCache(path.join(__dirname,'public'),{
+  maxAge : 60 * 60 * 31
+}));
 app.use(require('koa-static')(path.join(__dirname, 'public')));
 app.use(views(path.join(__dirname, 'views'), {
   map: {html: 'swig'}
 }));
+app.use(gzip());
 app.use(require('./web_route').routes());
 
 app.listen(config.port);
